37afcb7ff9216323ddc0d6db595b6f7b9e2d3426
fealiamofili-lab <fealiam.ofili@csuglobal.edu>
2025-11-13 02:39:04 -0500

    Add files via upload
    Add ct1.py for submission

---
+++ ct1.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter your first number:\n"))
+num2 = int(input("Enter your second number:\n"))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+
+
